8eea3cfa14f609972b28fc75a38453c9d6287d2b
Slava V <leterthing@gmail.com>
2023-02-06 02:46:37 +0700

    added jwt auth service

---
+++ src/auth/auth.constants.ts
@@ -0,0 +1,3 @@
+export const  ALREDY_REGISTERED_ERROR = 'Такой пользователь уже существует';
+export const  USER_NOT_FOUND_ERROR = 'Пользователь не найден';
+export const  WRONG_PASSWORD_ERROR = 'Неверный пароль';
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { TypegooseModule } from 'nestjs-typegoose';
+import { jetJWTConfig } from 'src/configs/jwt.config';
 import { AuthController } from './auth.controller';
-import { AuthModel } from './auth.model/auth.model';
+import { UserModel } from './auth.model/user.model';
+import { AuthService } from './auth.service';
 
 @Module({
   controllers: [AuthController],
   imports: [
     TypegooseModule.forFeature([
       {
-        typegooseClass: AuthModel,
+        typegooseClass: UserModel,
         schemaOptions: {
-          collection: 'Auth'
+          collection: 'User'
         }
       }
-    ])
-  ]
+    ]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: jetJWTConfig
+    })
+  ],
+  providers: [AuthService]
 })
 export class AuthModule {}
